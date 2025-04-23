be541e365885495972d2b93972d6a280447eb212
Cameron Malone <cameronmkm2@gmail.com>
2025-04-23 13:07:42 +1000

    feat: main page nav fonts and icons
    Update the main page navbar / header to use the new fonts and icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,25 @@
 'use client';
 
+import Clubs from '@/components/icons/Clubs';
+import Diamonds from '@/components/icons/Diamonds';
+import Hearts from '@/components/icons/Hearts';
+import Spades from '@/components/icons/Spades';
 import SearchBar from '@/components/SearchBar';
 import Link from 'next/link';
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-20 items-center justify-items-center min-h-screen p-8">
-      <nav>
-        <h1 className="text-3xl font-mono font-bold text-white">
-          unshuffle.com &#x1F0A0;
-        </h1>
+      <nav className="flex flex-col gap-2">
+        <h1 className="text-5xl font-header font-bold text-white">unshuffle</h1>
+        <div className="flex gap-1 items-center justify-center">
+          <Clubs size={50} />
+          <Spades size={50} />
+          <Hearts size={50} />
+          <Diamonds size={50} />
+        </div>
       </nav>
-      <main className="flex flex-col gap-20 items-center justify-center w-full max-w-prose">
-        <SearchBar />
-
-        <Link
-          href={'holdem'}
-          className="font-mono font-bold py-2 px-4 rounded bg-white hover:bg-slate-100"
-        >
-          Texas Hold'em
-        </Link>
-      </main>
+      <main className="flex flex-col gap-20 items-center justify-center w-full max-w-prose"></main>
     </div>
   );
 }
