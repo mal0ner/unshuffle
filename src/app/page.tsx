82a9f8cda692d206d39b106c3b290bd21891dafb
JayB <jay.butterworth.tps@gmail.com>
2025-05-07 13:46:41 +1000

    style: responsive-design-for-mainpage-header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
   return (
     <div className="flex flex-col gap-20 items-center justify-items-center min-h-screen p-8">
       <nav className="flex flex-col gap-2">
-        <h1 className="text-5xl font-header font-bold text-white">unshuffle</h1>
+        <h1 className="text-4xl md:text-5xl hover:text-accent-red font-header font-bold text-white">unshuffle</h1>
         <div className="flex gap-1 items-center justify-center">
           <Suit suit="club" size={50} />
           <Suit suit="spade" size={50} />
